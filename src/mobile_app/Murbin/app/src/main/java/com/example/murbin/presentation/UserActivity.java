f7df3cdafdd8c437b6d39bdac5145e32ae6d385e
Franscisco Javier Paños Mardrona <frapaoma@epsg.upv.es>
2020-10-30 00:09:39 +0100

    feat: Optimized the code and added the personalized login with email

--- src/mobile_app/Murbin/app/src/main/java/com/example/murbin/presentation/UserActivity.java
+++ src/mobile_app/Murbin/app/src/main/java/com/example/murbin/presentation/UserActivity.java
@@ -3,8 +3,8 @@ package com.example.murbin.presentation;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import com.example.murbin.CustomApplication;
 import com.example.murbin.R;
 
 public class UserActivity extends AppCompatActivity {
@@ -14,9 +14,18 @@ public class UserActivity extends AppCompatActivity {
      */
     private static final String TAG = UserActivity.class.getSimpleName();
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_activity);
+
+        mToolbar = findViewById(R.id.user_activity_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_atras);
+        getSupportActionBar().setTitle("");
     }
 }
