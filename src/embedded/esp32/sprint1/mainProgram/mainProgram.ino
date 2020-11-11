ed4e355a5f925a906ef1fb5d10d742d7f2befede
Abidán Brito Clavijo <abidan.brito@gmail.com>
2020-11-11 11:52:57 +0100

    fix: modified directory structure and filename for clarity

--- src/embedded/esp32/sprint1/mainProgram/mainProgram.ino
+++ src/embedded/esp32/sprint1/mainProgram/mainProgram.ino
@@ -1,75 +1,79 @@
-
+//#include <M5Stack.h>
 //PROGRAMA ACTUADORES //
-
-int sensorCO2 = 34;    
-int sensorLUM = 35; 
-const int PIRPin = 36; // pin de entrada (para el sensor PIR) 
-
-
-#define LED   5 //defino el led como pin 36
-
+ 
+const int sensorCO2 = 2;    
+const int sensorLUM = 35;
+const int PIRPin = 36; // pin de entrada (para el sensor PIR)
+ 
+#define LED 5 //defino el led como pin 5
+ 
 #define LUM_SENSOR  'L'       // defino el primer sensor de manera escalable
 #define CO2_SENSOR  'C'       // defino el segundo sensor de manera escalable
 #define PIR_SENSOR  'P'       // defino el tercer sensor de manera escalable
-
+ 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  //Serial.begin(115200);
   //Configurando los pines de entrada y salida
-  pinMode(PIRPin, INPUT);   
+  pinMode(PIRPin, INPUT);  
   pinMode(sensorCO2, INPUT);       // Sensor CO2
   pinMode(sensorLUM, INPUT);       // Sensor LUZ
   pinMode(LED, OUTPUT);
+  M5.begin();
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.print("Pin GPIO value: ");
 }
-
+ 
 void loop() {
-
-  
+ 
+ 
    if (Serial.available() > 0) {
-      char command = (char) Serial.read(); 
+      char command = (char) Serial.read();
       switch (command) {
          case CO2_SENSOR:
-         Serial.print("Enviando informacion.. "); 
+         Serial.print("Enviando informacion.. ");
             sensor_CO2(sensorCO2, LED);
             break;
-         case LUM_SENSOR: 
-         Serial.print("Enviando informacion.. "); 
+         case LUM_SENSOR:
+         Serial.print("Enviando informacion.. ");
           Serial.print(String(sensorLUM));
          sensor_LUM(sensorLUM, LED);
-            break; 
-         case PIR_SENSOR: 
-         Serial.print("Enviando informacion.. "); 
+            break;
+            /*
+         case PIR_SENSOR:
+         Serial.print("Enviando informacion.. ");
          Serial.print(String(PIRPin));
             sensor_PIR(PIRPin, LED);
             break;
-            
-      } 
+            */
+           
+      }
    }
 }
-
+ 
 //programa sensor Co2
 void sensor_CO2(int pin_mq, int ledPIN){
-  
+ 
   boolean mq_estado = digitalRead(pin_mq);//Leemos el sensor
-  
+ 
   if(mq_estado) //si la salida del sensor es 1
   {
     digitalWrite(ledPIN , LOW);    // poner el Pin en LOW
     Serial.println("Sin presencia de alcohol");
-    
+   
   }
   else //si la salida del sensor es 0
   {
     digitalWrite(ledPIN , HIGH);   // poner el Pin en HIGH
     Serial.println("Alcohol detectado");
-    delay(2000); 
+    delay(2000);
     digitalWrite(ledPIN , LOW);
   }
 }
-
+ 
 //programa sensor luz
 void sensor_LUM(int pin, int led){
-  int valor = analogRead(pin_mq);
+  int valor = analogRead(pin);
   Serial.println(valor);
   //si el valor de la luz es mayor a 1000, el led se enciende y se apagara a los 2 segundos
   if(valor > 1000){
@@ -82,16 +86,17 @@ void sensor_LUM(int pin, int led){
     digitalWrite(led, LOW);
   }
 }
-
+ 
 //programa sensor luz
+/*
 void sensor_PIR(int PIRPin, int LEDpin){
-  
+ 
    val = digitalRead(PIRPin); //Lectura de datos
    if (val == HIGH) { //si está activado
      
       digitalWrite(LEDPin, HIGH);  //LED ON
       if (pirState == LOW){ //si previamente estaba apagado
-      
+     
         Serial.println("Sensor activado");
         pirState = HIGH;
       }
@@ -99,31 +104,32 @@ void sensor_PIR(int PIRPin, int LEDpin){
    
       digitalWrite(LEDPin, LOW); // LED OFF
       if (pirState == HIGH){  //si previamente estaba encendido
-      
+     
         Serial.println("Sensor parado");
         pirState = LOW;
       }
    }
 }
+*/
 /*
 //PROGRAMA UTILIZADO PARA M5STACK
-
-
+ 
+ 
 #include <M5Stack.h>
-
+ 
 const int ledPIN = 5;
 int pin_mq = 2;
-
-void setup() { 
+ 
+void setup() {
   Serial.begin(9600);
  // M5.begin();
  // M5.Lcd.setTextSize(2);
  // M5.Lcd.print("Pin GPIO value: ");
   pinMode(pin_mq, INPUT);
-  pinMode(ledPIN , OUTPUT); 
+  pinMode(ledPIN , OUTPUT);
 }
-
-
+ 
+ 
 void loop() {
   int valor = analogRead(pin_mq);
   boolean mq_estado = digitalRead(pin_mq);//Leemos el sensor
@@ -133,13 +139,13 @@ void loop() {
     //M5.Lcd.println("Sin presencia de alcohol");
     digitalWrite(ledPIN , LOW);    // poner el Pin en LOW
     Serial.println("Sin presencia de alcohol");
-    
+   
   }
   else //si la salida del sensor es 0
   {
     digitalWrite(ledPIN , HIGH);   // poner el Pin en HIGH
     Serial.println("Alcohol detectado");
   }
-  delay(2000); 
+  delay(2000);
 }
 */
