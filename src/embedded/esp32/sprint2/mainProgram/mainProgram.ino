b21792b087b14821c69822df8a0f43561842400d
Abidán Brito Clavijo <abidan.brito@gmail.com>
2020-11-16 18:15:04 +0100

    feat: added co2 and luminosity sensors calibration

--- src/embedded/esp32/sprint2/mainProgram/mainProgram.ino
+++ src/embedded/esp32/sprint2/mainProgram/mainProgram.ino
@@ -41,50 +41,54 @@ void loop() {
             Serial.println("-- LUM SENSOR --");
             sensor_LUM(sensorLUM, LED_LUM);
          }
-         break; 
-      default:
-          break;
       }
    }   
 }
 
-//
-//
-//
 void sensor_CO2(int pin, int ledPin){
   int value = analogRead(pin);
-  if (value < 1780) 
-  {
-    Serial.print("Nivel alto de CO2: ");
+  if (value > 2750) {
+    Serial.print("Nivel alto de CO2 - ");
+    Serial.println(value);
     digitalWrite(ledPin , HIGH);  
-    delay(2500);
+    delay(1000);
     digitalWrite(ledPin , LOW);
   }
-
-  else
-  {
-    Serial.print("Nivel medio de C02: ");
+  else {
+    Serial.print("Nivel medio de C02 - ");
+    Serial.println(value);
   }
-  Serial.println(value);
 }
  
 void sensor_LUM(int pin, int ledPin){
   int valor = analogRead(pin);
+  float voltage_value = valor * 3.3 / 4095.0;
 
-  if (valor < 3050){
-    Serial.print("Hay luz intensa: ");
+  if (valor <= 170){
+    Serial.print("Nivel de luz 0 - ");
   }
   else {
-    if (valor > 3050 && valor < 3220){
-      Serial.print("Hay luz ambiental: ");
+    if (valor > 170 && valor <= 400){
+      Serial.print("Nivel de luz 1 - ");
+    }
+    else if (valor > 400 && valor <= 700){
+      Serial.print("Nivel de luz 2 - ");
+    }
+    else if (valor > 700 && valor <= 1000){
+      Serial.print("Nivel de luz 3 - ");
+    }
+    else if (valor > 1000 && valor <= 1500){
+      Serial.print("Nivel de luz 4 - ");
+    }
+    else if (valor > 1500 && valor <= 2100){
+      Serial.print("Nivel de luz 5 - ");
     }
     else {
-      Serial.print("No hay luz: ");
+      Serial.print("Nivel de luz 6 - ");
     }
+    Serial.println(valor);
     digitalWrite(ledPin, HIGH);
-    delay(2500);
+    delay(1000);
     digitalWrite(ledPin, LOW);
   }
-     
-  Serial.println(valor);
 }
