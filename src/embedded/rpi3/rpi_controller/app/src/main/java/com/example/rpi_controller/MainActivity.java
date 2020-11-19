565c40011dc9d28bdc10347ed73d2f990022bc4b
Yeray <ycansam@epsg.upv.es>
2020-11-19 10:57:39 +0100

    feat: mqtt

--- src/embedded/rpi3/rpi_controller/app/src/main/java/com/example/rpi_controller/MainActivity.java
+++ src/embedded/rpi3/rpi_controller/app/src/main/java/com/example/rpi_controller/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.rpi_controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.comun.Mqtt;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 /**
  * Skeleton of an Android Things activity.
@@ -22,10 +30,14 @@ import android.os.Bundle;
  * You can find additional examples on GitHub: https://github.com/androidthings
  */
 public class MainActivity extends AppCompatActivity {
-
+    public static MqttClient client = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
 }
