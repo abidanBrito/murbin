cd49a737c6be42d13c16bf7412cdbfa05998f309
Abidán Brito Clavijo <abidan.brito@gmail.com>
2020-11-19 22:31:53 +0100

    feat: proper uart data sending approach

--- src/embedded/esp32/sprint2/mainProgram/sensor_CO2.cpp
+++ src/embedded/esp32/sprint2/mainProgram/sensor_CO2.cpp
@@ -5,10 +5,10 @@
 
 void sensor_CO2(int pin, int ledPin){
   int value = analogRead(pin);
-  if (value < 1780) 
-  {
+  
+  if (value > 2850) {
     digitalWrite(ledPin , HIGH);  
-    delay(2500);
+    delay(800);
     digitalWrite(ledPin , LOW);
   }
   Serial.println(value);
--- src/embedded/esp32/sprint2/mainProgram/sensor_LUZ.cpp
+++ src/embedded/esp32/sprint2/mainProgram/sensor_LUZ.cpp
@@ -5,10 +5,10 @@
 void sensor_LUM(int pin, int ledPin){
   int valor = analogRead(pin);
 
-  if (valor > 800){
+  if (valor > 800) {
     digitalWrite(ledPin, HIGH);
-    delay(1000);
+    delay(800);
     digitalWrite(ledPin, LOW);
-    Serial.println(valor);
   }
+    Serial.println(valor);
 }
