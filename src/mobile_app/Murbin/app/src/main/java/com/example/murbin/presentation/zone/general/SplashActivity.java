4837c3fb9f66a0bd15fe3c43f28e54ed07c6024d
Losina <losinagfx@gmail.com>
2021-01-21 09:23:41 +0100

    feat: Splash activity and streetlight map icon

---
+++ src/mobile_app/Murbin/app/src/main/java/com/example/murbin/presentation/zone/general/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.murbin.presentation.zone.general;
+
+import android.os.Bundle;
+
+import com.example.murbin.BaseActivity;
+import com.example.murbin.R;
+import com.example.murbin.uses_cases.UsesCasesZoneGeneral;
+
+public class SplashActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.splash_activity);
+    }
+
+}
